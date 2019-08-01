27c3de74e50b200949aea3791f8f0d14b8bf5d88
Shantanu Kushwaha <shantanusk@hotmail.com>
2019-08-01 08:00:09 +0530

    Codeigniter Basici Setup With Bootstrap 4 And Fontawesome 5

---
+++ application/modules/welcome/controllers/Welcome.php
@@ -0,0 +1,66 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Welcome extends MY_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('welcome_message');
+	}
+
+	/*
+	$this->load->library("pagination");
+    $config = array();
+        $config["base_url"] = base_url() . "admin_stories/list";
+        $config["total_rows"] = $this->ASM->get_stories_count();
+        $config["per_page"] = 10;
+        $config["uri_segment"] = 3;
+        $config['full_tag_open'] = "<ul class='pagination'>";
+        $config['full_tag_close'] ="</ul>";
+        $config['num_tag_open'] = '<li>';
+        $config['num_tag_close'] = '</li>';
+        $config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+        $config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+        $config['next_tag_open'] = "<li>";
+        $config['next_tagl_close'] = "</li>";
+        $config['prev_tag_open'] = "<li>";
+        $config['prev_tagl_close'] = "</li>";
+        $config['first_tag_open'] = "<li>";
+        $config['first_tagl_close'] = "</li>";
+        $config['last_tag_open'] = "<li>";
+        $config['last_tagl_close'] = "</li>";
+        $this->pagination->initialize($config);
+
+        $page = ($this->uri->segment(3)) ? $this->uri->segment(3) : 0;
+
+        $data["links"] = $this->pagination->create_links();
+
+        $data['result'] = $this->ASM->get_stories_pagination($config["per_page"], $page);
+		$data['page'] = "Stories List / View";
+		$data['message'] = "";
+    	$data['obj'] = $this;
+	 public function get_stories_pagination($limit, $start) {
+        $this->db->limit($limit, $start);
+        $query = $this->db->get("st_stories");
+
+        return $query->result_array();
+    }
+
+
+	*/
+}
